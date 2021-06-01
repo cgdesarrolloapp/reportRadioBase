4c4d3cb80e6a6a992c491ee8550563927caabb77
cgdesarrolloapp <cesar.rodriguez@sofoscorp.com>
2021-05-31 23:30:46 -0500

    ajuste

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // Make BootstrapVue available throughout your project
-App.use(BootstrapVue)
+createApp.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
-App.use(IconsPlugin)
+createApp.use(IconsPlugin)
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
